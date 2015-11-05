f6571005a2b4c88bdbacecfe07a20b35679cc97f
Gavin Goroski <gavin.goroski@gmail.com>
2015-11-04 19:19:51 -0700

    first script

---
+++ Assets/Scripts/GameManger.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManger : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		BeginGame();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.R)) {
+			RestartGame();
+		}
+	
+	}
+
+	private void BeginGame ()
+	{
+
+	}
+
+	private void RestartGame () 
+	{
+		BeginGame ();
+	}
+}
