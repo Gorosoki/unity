5b32b534fcad45d06d78fe806f073ed2aed67476
Gavin Goroski <gavin.goroski@gmail.com>
2015-11-28 15:20:21 -0700

    added in game menu

--- Assets/Scripts/GameManger.cs
+++ Assets/Scripts/GameManger.cs
@@ -1,15 +1,17 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
 
 
 
 public class GameManger : MonoBehaviour {
 
     public Camera OverviewCamera;
+	private bool paused = false;
     // Use this for initialization
     void Start () {
-		BeginGame();
         OverviewCamera.enabled = false;
+		Time.timeScale = 1f;
 	
 	}
 	
@@ -22,17 +24,55 @@ public class GameManger : MonoBehaviour {
         {
             OverviewCamera.enabled = !OverviewCamera.enabled;
         }
+		if (Input.GetKeyDown ("escape")) {
+			paused = togglePause ();
+		}
 	
 	}
-
-	private void BeginGame ()
+	void OnGUI()
 	{
-
+		if (paused) {
+			GUILayout.BeginArea(new Rect(0, 0, Screen.width, Screen.height));
+			GUILayout.FlexibleSpace();
+			GUILayout.BeginHorizontal();
+			GUILayout.FlexibleSpace();
+			
+			GUILayout.BeginVertical("box");
+			GUILayout.Label ("Game is paused!");
+			if (GUILayout.Button ("Continue")) {
+				paused = togglePause ();
+			}
+			if (GUILayout.Button ("Restart")) {
+				Application.LoadLevel(Application.loadedLevel);
+				
+			}
+			if (GUILayout.Button ("Exit")) {
+				Application.Quit ();
+			}
+			GUILayout.EndVertical();		
+			GUILayout.FlexibleSpace();
+			GUILayout.EndHorizontal();
+			GUILayout.FlexibleSpace();
+			GUILayout.EndArea();
+		}
 	}
 
 	private void RestartGame () 
 	{
-		BeginGame ();
         Application.LoadLevel(Application.loadedLevel);
+	}
+
+	private bool togglePause()
+	{
+		if(Time.timeScale == 0f)
+		{
+			Time.timeScale = 1f;
+			return(false);
+		}
+		else
+		{
+			Time.timeScale = 0f;
+			return(true);    
+		}
 	}
 }
