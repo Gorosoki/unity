79ac6202332c817e3faef67a4f170a294ad2435f
Gavin Goroski <gavin.goroski@gmail.com>
2015-11-28 19:50:52 -0700

    cleanup

--- Assets/Scripts/GameManger.cs
+++ Assets/Scripts/GameManger.cs
@@ -24,7 +24,7 @@ public class GameManger : MonoBehaviour {
         {
             OverviewCamera.enabled = !OverviewCamera.enabled;
         }
-		if (Input.GetKeyDown ("escape")) {
+		if (Input.GetKeyDown (KeyCode.P)) {
 			paused = togglePause ();
 		}
 	
