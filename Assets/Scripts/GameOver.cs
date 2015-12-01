d56a136c8ff378e4f82b47a6c05d3ee100ece092
Gavin Goroski <gavin.goroski@gmail.com>
2015-11-30 21:20:27 -0700

    more game over

---
+++ Assets/Scripts/GameOver.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameOver : MonoBehaviour {
+
+    void OnCollisionEnter (Collision col)
+    {
+        if (col.gameObject.name == "Player")
+        {
+            GameManger gameManger = GetComponent<GameManger>();
+            gameManger.gameOverToggle();
+            print("Game Over!");
+        }
+    }
+}
